61ee7edeccd6be6f8971086e8e25b996c0752c94
DaVor05 <davidvid1201@gmail.com>
2026-05-25 10:39:16 -0700

    Crecion de intefraz y uso de intefraz desde otro archivo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Task } from '../models/task.models';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,25 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   styleUrls: ['home.page.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
+
 export class HomePage {
-  constructor() {}
+  tasks: Task[]=[
+    {
+      id: 1, titulo: 'Configuracion de Ionic',
+      descripcion: 'Instalar Node.js, Angular CLI e Ionic CLI',
+      finalizado: false,
+      prioridad: 'Alta'
+    }, 
+    {
+      id: 2,
+      titulo: 'Crear app tasklist',
+      descripcion: 'Crear proyecto inicial con Ionic CLI',
+      finalizado: false,
+      prioridad: 'Alta'
+    },];
+
+  constructor(){
+    console.log(this.tasks);
+  }
+
 }
