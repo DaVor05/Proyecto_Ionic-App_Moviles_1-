6cdf7d60183fdb3612cc209f2e95788bc639c225
DaVor05 <davidvid1201@gmail.com>
2026-05-28 11:30:52 -0700

    Agregacion de Button, Label, Input para Task y NewTaskStr

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {FormsModule} from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton, IonList } from '@ionic/angular/standalone';
 import { Task } from '../models/task.models';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton, IonList, FormsModule],
 })
 
 export class HomePage {
-  tasks: Task[]=[
+
+  newTaskStr: string = '';
+
+  tasks: Task[] = [
     {
-      id: 1, titulo: 'Configuracion de Ionic',
+      id: 1, 
+      titulo: 'Configuracion de Ionic',
       descripcion: 'Instalar Node.js, Angular CLI e Ionic CLI',
       finalizado: false,
       prioridad: 'Alta'
@@ -23,10 +28,23 @@ export class HomePage {
       descripcion: 'Crear proyecto inicial con Ionic CLI',
       finalizado: false,
       prioridad: 'Alta'
-    },];
+    },
+  ];
 
-  constructor(){
+  constructor() {
     console.log(this.tasks);
   }
 
+  addTask() {
+    console.log(this.newTaskStr);
+    const newTask: Task = {
+      id: Date.now(),
+      titulo: this.newTaskStr,
+      descripcion: '',
+      finalizado: false,
+      prioridad: 'Media'
+    };
+    this.tasks.push(newTask);
+    console.log(this.tasks);
+  }
 }
