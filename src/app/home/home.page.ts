c21f5cc31be0d9cb058e689cea8c7b640d061f93
DaVor05 <davidvid1201@gmail.com>
2026-05-31 14:35:28 -0700

    Las 4 Validaciones Agregadas a la App

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,26 @@
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton, IonList } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton, IonList, IonAlert } from '@ionic/angular/standalone';
 import { Task } from '../models/task.models';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton, IonList, FormsModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton, IonList, IonAlert, FormsModule],
 })
-
 export class HomePage {
 
   newTaskStr: string = '';
 
+  isAlertOpen: boolean = false;
+  alertMessage: string = '';
+  alertButtons: string[] = ['Entendido'];
+
   tasks: Task[] = [
     {
       id: 1, 
-      titulo: 'Configuracion de Ionic',
+      titulo: 'Configuracion de Ionic Basica',
       descripcion: 'Instalar Node.js, Angular CLI e Ionic CLI',
       finalizado: false,
       prioridad: 'Alta'
@@ -35,16 +38,45 @@ export class HomePage {
     console.log(this.tasks);
   }
 
+  //Alerta de Error
+  showAlert(message: string) {
+    this.alertMessage = message;
+    this.isAlertOpen = true;
+  }
+
   addTask() {
-    console.log(this.newTaskStr);
+    console.log('Valor recibido:', this.newTaskStr);
+
+    //4.Espacios en blanco antes y después.
+    const cleanTitle = this.newTaskStr.trim();
+
+    //1.Título: No vacío.
+    if (cleanTitle === '') {
+      this.showAlert('El título de la tarea no puede estar vacío o contener solo espacios.');
+      return;
+    }
+
+    //2 y 3.No duplicados & Sensible a mayúsculas y minúsculas.
+    const isDuplicate = this.tasks.some(
+      (task) => task.titulo.trim().toLowerCase() === cleanTitle.toLowerCase()
+    );
+
+    if (isDuplicate) {
+      this.showAlert('¡Esta tarea ya existe en tu lista! No se permiten duplicados.');
+      return;
+    }
+
     const newTask: Task = {
       id: Date.now(),
-      titulo: this.newTaskStr,
+      titulo: cleanTitle,
       descripcion: '',
       finalizado: false,
       prioridad: 'Media'
     };
+
     this.tasks.push(newTask);
-    console.log(this.tasks);
+    console.log('Lista actualizada:', this.tasks);
+
+    this.newTaskStr = '';
   }
 }
